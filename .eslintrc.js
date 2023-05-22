4afc4b1dbb5bd5bd75a3a337c20d081a0ee8cc41
Gleb <glebbong12387@gmail.com>
2023-05-22 14:39:57 +0300

    init repo, add  movie questions

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "amd": true,
+        "node": true
+    },
+    "extends": "eslint:recommended",
+    "overrides": [
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "rules": {
+        "semi": [2, "always"]
+        // "camelcase" : true,
+        // "indent": 2,
+        // "undef": true,
+        // "quotmark": false,
+        // "maxlen": 80,
+        // "trailing": true,
+        // "curly": true,
+        // "strict": false,
+        // "browser": true,
+        // "devel": true,
+        // "jquery": true,
+        // "esversion": 6
+    }
+};
