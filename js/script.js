1f515765ce8324555cc046727affbf84a7bb4ba0
Gleb <glebbong12387@gmail.com>
2023-05-31 19:41:34 +0300

    change all functions into object methods

--- js/script.js
+++ js/script.js
@@ -2,97 +2,98 @@
 "use strict";
 
 
-//-----------------------------------------------SWITCH (alternative IF ELSE construction)
-// if (1) {
-//     console.log('Ok');
-// }else{
-//     console.log('Error');
-// }
-
-
-// if (num < 49) {
-//     console.log('Error');
-// }else if (num > 100) {
-//     console.log("Too much");
-// }else {
-//     console.log("Ok!");
-// }
-
-// ------
-
-// (num === 50) ? console.log("Ok!") : console.log('Error');
-// const num = 50;
-
-// --------
-
-// const num = 50;
-
-// switch (num) {
-    //     case 49:
-    //         console.log('Error');
-//         break;
-//     case 100:
-//         console.log('Error');
-//         break;
-//     case 51:
-//         console.log('Good');
-//         break;
-//     default:
-//         console.log('Not this time');
-//      break;
-//     }
-
-// -------------------------------------------------------------------------------Loops (циклы)----------------------------------
-// let num = 50;
-
-// while (num <= 55) {
-    //     console.log(num);
-    //     num++;
-    // }
-
-    // ------
-
-    // do {
-//     console.log(num);
-//     num++;
-// }
-// while (num < 55);
-
-// -------
-
-// for (let i = 1; i < 10; i++){
-//     if (i === 6) {
-//         break; - прерывает цикл если текущая итерация РАВНА значению 6
-//         continue; - пропускает итерацию которая РАВНА значению и ПРОДОЛЖАЕТ цикл.
-//     }
-//     console.log(i);
-// }
-
-
-
-
 
 // ---------------------------------------FILMS QUESTIONER-------------------------------------------------------
 
-let numberOfFilms;
 
-function start () {
-    numberOfFilms = +prompt("Скок фильмов?", "");
-
-    while (numberOfFilms == "" || numberOfFilms == null || isNaN(numberOfFilms)) {
-        numberOfFilms = +prompt("Скок фильмов?", "");
-    }
-}
-start();
 
 
 
 const personalMovieDB = {
-    count: numberOfFilms,
+    count: 0,
     movies: {},
     actors: {},
     genres: [],
-    privat: false
+    privat: false,
+    start: function() {
+        personalMovieDB.count = +prompt("Скок фильмов?", "");
+
+        while (personalMovieDB.count == "" || personalMovieDB.count == null || isNaN(personalMovieDB.count)) {
+            personalMovieDB.count = +prompt("Скок фильмов?", "");
+        }
+    },
+    rememberMyFIlms: function() {
+        for ( let i = 0; i < 2; i++) {
+    const a = prompt("Один из последних просмотренных фильмов?", ""),
+        b = prompt("На сколько оцените его?", "");
+
+
+        if (a != null && b != null && a != "" && b  != "" && a.length < 50) {
+            personalMovieDB.movies[a] = b;
+            console.log("Done");
+        } else {
+                console.log("error");
+                i--;
+            }
+        }
+    },
+    detectPersonalLevel: function(){
+
+        if (personalMovieDB.count <= 10 ){
+            console.log("Просмотрено довольно мало фильмов");
+        } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30){
+            console.log("Вы классический зритель");
+        }else if (personalMovieDB.count >= 30){
+            console.log("Вы классический киноман");
+        }else{
+            console.log("error");
+        }
+
+    },
+    showMyDB: function(hidden){
+        if (!hidden) {
+            console.log(personalMovieDB);
+        }
+    },
+    toggleVisibleMyDB: function(){
+        if (personalMovieDB.privat){
+            personalMovieDB.privat = false;
+        }else{
+            personalMovieDB.privat = true;
+        }
+   
+    },
+    writeYourGenres: function() {
+        for (let i = 1; i <= 3; i++) {
+          let genre = prompt(`Ваш любимый жанр под номером ${i}`);
+          if (genre != '' && genre != null) {
+            personalMovieDB.genres[i - 1] = genre;
+          } else {
+            console.log('error');
+            i--;
+          }
+        }
+        personalMovieDB.genres.forEach((item, i) => {
+           console.log( `Любимый жанр ${i + 1} - это ${item}`);
+
+        });
+      }
+    
+   // writeYourGenres: function(){
+    //     for (let i = 1; i <= 3; i++) {
+    //         if(personalMovieDB.genres != '' && personalMovieDB.genres != null){
+    //             personalMovieDB.genres[i - 1] = prompt(`"Ваш любимый жанр под номером ${i}"`);
+    //         }else{
+    //             console.log('error');
+    //             i--;
+    //         }
+    //     }
+    // }
+                
+
+
+
+
 };
 
 //----------------------------alternative loops--------------
@@ -130,58 +131,11 @@ const personalMovieDB = {
             //     i++;
             // }
 
-function rememberMyFIlms() {
-                for ( let i = 0; i < 2; i++) {
-            const a = prompt("Один из последних просмотренных фильмов?", ""),
-                b = prompt("На сколько оцените его?", "");
-    
-    
-                if (a != null && b != null && a != "" && b  != "" && a.length < 50) {
-                    personalMovieDB.movies[a] = b;
-                    console.log("Done");
-                } else {
-                        console.log("error");
-                        i--;
-                    }
-        }
-    }
-
-// rememberMyFIlms();
-
-function detectPersonalLevel(){
-
-            if (personalMovieDB.count <= 10 ){
-                console.log("Просмотрено довольно мало фильмов");
-            } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30){
-                console.log("Вы классический зритель");
-            }else if (personalMovieDB.count >= 30){
-                console.log("Вы классический киноман");
-            }else{
-                console.log("error");
-            }
-
-        }
-
-// detectPersonalLevel();
-
-function showMyDB (hidden){
-    if (!hidden) {
-        console.log(personalMovieDB);
-    }
-
-}
-
-showMyDB(personalMovieDB.privat);
-
-function writeYourGenres (){
-    for (let i = 1; i <= 3; i++) {
-        personalMovieDB.genres[i - 1] = prompt(`"Ваш любимый жанр под номером ${i}"`);
-
-    }
+ 
 
 
-}
-writeYourGenres();
+
+
 
 
 
